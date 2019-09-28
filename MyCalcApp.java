fbcc2b7cabcc3efcbfb6c36152f5f1a8e50482fa
kasturiphalak <54503694+kasturiphalak@users.noreply.github.com>
2019-09-28 20:22:17 +0530

    Add files via upload

---
+++ MyCalcApp.java
@@ -0,0 +1,20 @@
+package assignmentexception;
+
+import java.util.Scanner;
+
+public class MyCalcApp 
+{
+	int caldouble()
+	{
+	Scanner sc=new Scanner(System.in);
+	b=sc.nextInt();
+
+
+int c;
+c=b*2;
+return c;
+	
+}
+
+
+}
